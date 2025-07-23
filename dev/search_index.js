c09c19534f245687aaf0fa241aee0fddfe87e68b
Documenter.jl <documenter@juliadocs.github.io>
2025-07-23 19:59:23 +0000

    build based on cf70066

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#PSDTFE","page":"Home","title":"PSDTFE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PSDTFE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#PSDTFE","page":"Home","title":"PSDTFE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PSDTFE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#PSDTFE.BVH","page":"Home","title":"PSDTFE.BVH","text":"Explanation \n\n\n\n\n\n","category":"type"},{"location":"#PSDTFE.SimBox","page":"Home","title":"PSDTFE.SimBox","text":"Doc string\n\n\n\n\n\n","category":"type"}]
 }
